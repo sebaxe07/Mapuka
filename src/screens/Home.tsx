f3bcbfea9307f2e25a497db04d977fc1f549357a
sebaxe07 <sebaxe09@gmail.com>
2024-12-25 17:44:18 +0100

    Maps testing

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,9 @@ import { Button } from "@react-navigation/elements";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
+import Logo from "../../assets/images/ISOTIPO (2).svg";
+import MapView, { UrlTile } from "react-native-maps";
+import Mapbox from "@rnmapbox/maps";
 
 interface HomeProps {}
 
@@ -10,10 +13,27 @@ const Home: React.FC<HomeProps> = ({}) => {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Mapbox.MapView style={{ flex: 1 }} />
+      <Logo width={120} height={40} fill={"green"} />
+      <Text>Home </Text>
       <Button onPress={() => navigation.navigate("Details")}>
         Go to Details
       </Button>
+      <MapView
+        style={{ width: 200, height: 200 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <UrlTile
+          urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maximumZ={11} // Adjust zoom level as needed
+          flipY={true} // Ensure correct tile orientation
+        />
+      </MapView>
     </View>
   );
 };
