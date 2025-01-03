84087a7f31d7a98da0d5b58a5635a04eaf495e4c
sebaxe07 <sebaxe09@gmail.com>
2025-01-03 15:28:29 +0100

    Testing

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,28 +1,41 @@
 import { Button } from "@react-navigation/elements";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import Logo from "../../assets/images/ISOTIPO (2).svg";
 import MapView, { UrlTile } from "react-native-maps";
-import Mapbox from "@rnmapbox/maps";
+
+import { BlurView } from "expo-blur";
 
 interface HomeProps {}
-
-Mapbox.setAccessToken(
-  "pk.eyJ1IjoiY29kZWthdGFiYXR0bGUiLCJhIjoiY201NDUwazd5MTdlNDJvc2VvMjU1Z2dnMSJ9.H5y4T1mhK2AngJnnBYN_Bg"
-);
 
 const Home: React.FC<HomeProps> = ({}) => {
   const navigation = useNavigation();
 
+  const image = require("../../assets/images/mapBgDark.png");
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Logo width={120} height={40} fill={"green"} />
-      <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate("Details")}>
-        Go to Details
-      </Button>
-      <Button onPress={() => navigation.navigate("Map")}>Go to Map</Button>
+      <ImageBackground
+        source={image}
+        className="size-full flex-1 justify-center items-center"
+      >
+        <Logo width={120} height={40} fill={"green"} />
+        <Text>Home Screen</Text>
+        <Button onPress={() => navigation.navigate("Details")}>
+          Go to Details
+        </Button>
+        <Button onPress={() => navigation.navigate("Map")}>Go to Map</Button>
+        <BlurView
+          intensity={10}
+          tint="light"
+          className="h-20 w-4/5 rounded-2xl overflow-hidden justify-center items-center border border-white"
+          experimentalBlurMethod="dimezisBlurView"
+        >
+          <View>
+            <Text className="text-center text-white">Soy borroso</Text>
+          </View>
+        </BlurView>
+      </ImageBackground>
     </View>
   );
 };
--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,9 +1,10 @@
 import React, { Component, useEffect, useRef, useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View, Text } from "react-native";
 import MapboxGL, { UserLocation } from "@rnmapbox/maps";
-import LocationPuck from "@rnmapbox/maps";
 import * as Location from "expo-location";
 import { MAPBOX_ACCESS_TOKEN } from "@env";
+import { BlurView } from "expo-blur";
+import { Button, Input } from "react-native-elements";
 
 MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
@@ -59,25 +60,138 @@ const Map: React.FC<MapProps> = ({}) => {
     }
   };
 
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("text", text);
+  }, [text]);
+
+  const [markerLocation, setMarkerLocation] = useState<[number, number] | null>(
+    null
+  );
+  const [routeCoords, setRouteCoords] = useState<number[][] | null>(null);
+
+  const searchLocation = async () => {
+    console.log("Button pressed");
+    if (!text) return;
+
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(
+          text
+        )}&proximity=ip&access_token=${MAPBOX_ACCESS_TOKEN}`
+      );
+
+      const data = await response.json();
+
+      if (data.features && data.features.length > 0) {
+        const [longitude, latitude] = data.features[0].geometry.coordinates;
+
+        // Update the map camera to focus on the searched location
+        setMarkerLocation([longitude, latitude]);
+        if (cameraRef.current) {
+          cameraRef.current.setCamera({
+            centerCoordinate: [longitude, latitude],
+            zoomLevel: 14,
+            animationDuration: 1000,
+          });
+        }
+        traceRouteToMarker([longitude, latitude]);
+      } else {
+        Alert.alert("No results found", "Please try a different search term.");
+      }
+    } catch (error) {
+      console.error("Error searching location: ", error);
+      Alert.alert("Error", "Failed to fetch location. Please try again.");
+    }
+  };
+
+  const traceRouteToMarker = async (destination: [number, number]) => {
+    if (!userLocation) {
+      Alert.alert("User location not found", "Cannot trace a route.");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/walking/${userLocation[0]},${userLocation[1]};${destination[0]},${destination[1]}?alternatives=false&continue_straight=true&geometries=geojson&overview=full&steps=false&access_token=${MAPBOX_ACCESS_TOKEN}`
+      );
+
+      const data = await response.json();
+
+      if (data.routes && data.routes.length > 0) {
+        const route = data.routes[0].geometry.coordinates;
+        setRouteCoords(route);
+      } else {
+        Alert.alert("No route found", "Could not trace a route.");
+      }
+    } catch (error) {
+      console.error("Error tracing route: ", error);
+      Alert.alert("Error", "Failed to trace route. Please try again.");
+    }
+  };
+
   return (
     <View className="size-full flex-1 bg-blue-600 justify-center items-center">
-      <View className="size-full bg-fuchsia-400">
-        <MapboxGL.MapView
-          style={{ flex: 1 }}
-          styleURL="mapbox://styles/mapbox/dark-v11"
-          compassEnabled={true}
-        >
-          <MapboxGL.Camera ref={cameraRef} />
-          {UserLocation && (
-            <MapboxGL.LocationPuck
-              puckBearing={"course"}
-              pulsing={{ isEnabled: true, color: "blue", radius: "accuracy" }}
+      <MapboxGL.MapView
+        style={styles.map}
+        styleURL="mapbox://styles/codekatabattle/cm55m9p3i003b01po2yh31h59/draft"
+        compassEnabled={true}
+      >
+        <MapboxGL.Camera ref={cameraRef} />
+        {UserLocation && (
+          <MapboxGL.LocationPuck
+            puckBearing={"course"}
+            pulsing={{ isEnabled: true, color: "blue", radius: "accuracy" }}
+          />
+        )}
+        {markerLocation && (
+          <MapboxGL.PointAnnotation id="marker" coordinate={markerLocation}>
+            <View className="size-4 bg-red-500" />
+          </MapboxGL.PointAnnotation>
+        )}
+        {routeCoords && (
+          <MapboxGL.ShapeSource
+            id="routeSource"
+            shape={{
+              type: "Feature",
+              geometry: {
+                type: "LineString",
+                coordinates: routeCoords,
+              },
+              properties: {},
+            }}
+          >
+            <MapboxGL.LineLayer
+              id="routeLine"
+              style={{
+                lineWidth: 5,
+                lineColor: "#007AFF",
+              }}
             />
-          )}
-        </MapboxGL.MapView>
+          </MapboxGL.ShapeSource>
+        )}
+      </MapboxGL.MapView>
+
+      <View className="size-full relative items-center ">
+        <Input
+          placeholder="BASIC INPUT"
+          onChangeText={(value) => setText(value)}
+          value={text}
+        />
+        <Button title="Press me" onPress={searchLocation} />
       </View>
     </View>
   );
 };
 
 export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
