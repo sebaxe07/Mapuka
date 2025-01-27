35d85db45704bbb4e9db94815b859eb1bab7d070
sebaxe07 <sebaxe09@gmail.com>
2025-01-27 19:08:20 +0100

    mapFog types and bookmark loading

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -12,6 +12,7 @@ import NavbarBase from "../../assets/images/navbarbase.svg";
 import { colors } from "../../colors";
 import { Easing } from "react-native-reanimated";
 import { MotiView } from "moti";
+import Map from "./MapFog";
 
 MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
@@ -74,7 +75,7 @@ const Home: React.FC = ({ route }: any) => {
     () => (
       <>
         <MotiView
-          animate={{ translateY: changeTheme ? 0 : -60, scale: 0.9 }}
+          animate={{ translateY: changeTheme ? 0 : -60, scale: 0.8 }}
           transition={
             {
               type: "timing",
@@ -87,11 +88,11 @@ const Home: React.FC = ({ route }: any) => {
             className="bg-[#1a1b3f] rounded-full items-center justify-center size-14"
             onPress={handleMapCustom}
           >
-            <Icons.Layers color={colors.white} />
+            <Icons.Layers fill={"#E8EBFF"} />
           </TouchableOpacity>
         </MotiView>
         <MotiView
-          animate={{ translateY: changeTheme ? 0 : -120, scale: 0.9 }}
+          animate={{ translateY: changeTheme ? 0 : -120, scale: 0.8 }}
           transition={
             {
               type: "timing",
@@ -104,12 +105,12 @@ const Home: React.FC = ({ route }: any) => {
             className="bg-[#292929]  rounded-full items-center justify-center size-14"
             onPress={handleMapDark}
           >
-            <Icons.Layers color={colors.white} />
+            <Icons.Layers fill={"#5E5E5E"} />
           </TouchableOpacity>
         </MotiView>
 
         <MotiView
-          animate={{ translateY: changeTheme ? 0 : -180, scale: 0.9 }}
+          animate={{ translateY: changeTheme ? 0 : -180, scale: 0.8 }}
           transition={
             {
               type: "timing",
@@ -122,7 +123,7 @@ const Home: React.FC = ({ route }: any) => {
             className="bg-white  rounded-full items-center justify-center size-14"
             onPress={handleMapLight}
           >
-            <Icons.Layers color={colors.white} />
+            <Icons.Layers fill={"#AEAEAE"} />
           </TouchableOpacity>
         </MotiView>
       </>
@@ -139,8 +140,8 @@ const Home: React.FC = ({ route }: any) => {
 
   return (
     <View className="flex-1">
-      {/* Full-screen Map */
-      /* <Map
+      {/* Full-screen Map */}
+      <Map
         searchText={searchText}
         triggerAction={triggerAction}
         setTriggerAction={setTriggerAction}
@@ -166,7 +167,7 @@ const Home: React.FC = ({ route }: any) => {
                 className="bg-buttonPurple  rounded-full items-center justify-center size-14 z-10"
                 onPress={() => setChangeTheme(!changeTheme)}
               >
-                <Icons.Layers color={colors.white} />
+                <Icons.Layers fill={"#E8EBFF"} />
               </TouchableOpacity>
 
               {memoizedTheme}
