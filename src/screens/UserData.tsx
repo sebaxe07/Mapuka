9f0fa4044c58e22c047eda617e09eb4f82af19ed
Daniel <danielmr@uninorte.edu.co>
2025-02-02 01:31:44 +0100

    Alert Modal

--- src/screens/UserData.tsx
+++ src/screens/UserData.tsx
@@ -5,6 +5,7 @@ import { supabase } from "../utils/supabase";
 import { setUserData } from "../contexts/slices/userDataSlice";
 import Toast from "react-native-toast-message";
 import Button from "../components/Button";
+import AlertModal from "../components/AlertModal";
 
 const UserData: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -14,6 +15,10 @@ const UserData: React.FC = () => {
   const [lastName, setLastName] = useState(userData?.lastname || "");
   const [newFirstName, setNewFirstName] = useState(userData?.name || "");
   const [newLastName, setNewLastName] = useState(userData?.lastname || "");
+
+  const [loading, setLoading] = useState(false);
+  const [isAlertSaveVisible, setIsAlertSaveVisible] = useState(false);
+  const [isAlertCancelVisible, setIsAlerCancelVisible] = useState(false);
 
   const handleSave = async () => {
     if (!newFirstName.trim() || !newLastName.trim()) {
@@ -78,12 +83,42 @@ const UserData: React.FC = () => {
           label="Discard"
           special
           onPress={() => {
-            setNewFirstName(firstName);
-            setNewLastName(lastName);
+            setIsAlerCancelVisible(true);
           }}
         />
-        <Button label="Save" onPress={handleSave} />
+        <Button label="Save" onPress={() => setIsAlertSaveVisible(true)} />
       </View>
+
+      {/* Save Confirmation Modal */}
+      <AlertModal
+        isVisible={isAlertSaveVisible}
+        onBackdropPress={() => setIsAlertSaveVisible(false)}
+        message="Want to save the changes?"
+        onCancel={() => setIsAlertSaveVisible(false)}
+        onConfirm={() => {
+          handleSave();
+          setIsAlertSaveVisible(false);
+        }}
+        confirmText="Yes"
+        cancelText="Cancel"
+        loading={loading}
+      />
+
+      {/* Cancel Confirmation Modal */}
+      <AlertModal
+        isVisible={isAlertCancelVisible}
+        onBackdropPress={() => setIsAlerCancelVisible(false)}
+        message={`Want to discard the changes?\nAll unsaved changes will be lost.`}
+        onCancel={() => setIsAlerCancelVisible(false)}
+        onConfirm={() => {
+          setNewFirstName(firstName);
+          setNewLastName(lastName);
+          setIsAlerCancelVisible(false);
+        }}
+        confirmText="Yes"
+        cancelText="Cancel"
+        loading={loading}
+      />
     </View>
   );
 };
